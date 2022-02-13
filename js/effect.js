f6b1c4265e571f85de4fb5cd8e12bef6cbd1a4e7
git-dhruv <dhruvparikhrg@gmail.com>
2022-02-13 23:00:38 +0530

    mapped pages to animation automatically

--- js/effect.js
+++ js/effect.js
@@ -1,23 +1,18 @@
+/*
+AMM Packaging Company JS library
+Author: Dhruv Parikh
+Date: 13-02-2022
+Dependencies: JQuery, GSAP
+
+This is a sole property of Wolf Rayet Technologies. If anyone found using the file without permission of the author or company will be subjected to legal inquiries.
+*/  
 
 
-function detectMob() {
-    const toMatch = [
-        /Android/i,
-        /webOS/i,
-        /iPhone/i,
-        /iPad/i,
-        /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
-    ];
-    
-    return toMatch.some((toMatchItem) => {
-        return navigator.userAgent.match(toMatchItem);
-    });
-  }
-  
+/* Variables */
+var nav_redirect_url="index.html";
 
 function scaleUp(elem){
+    navmap(elem);
     gsap.to(elem,{
         duration:1,
         scaleX:1.5, 
@@ -40,6 +35,7 @@ function scaleDown(elem){
     }
     );
 }
+gsap.to(".yo", {duration: 5, text: "We love the Web and the work we do.We work closely with our clients to deliver </br>  the best possible solutions for their needs.", ease:"rough",repeat: -1, repeatDelay: 1});
 
 gsap.from(".reveal-later", {
     duration: 5,
@@ -51,6 +47,7 @@ gsap.from(".reveal-later", {
 
 
 function navclick(elem){
+    navmap(elem);
     var h = window.innerHeight/2;
     var w = -window.innerWidth/2;
     //window.scrollTo(0, 200);
@@ -93,6 +90,41 @@ function navclick(elem){
 }
 
 function redirect(){
-    window.location.href = "service.html";
+    window.location.href = nav_redirect_url;
 
 }
+
+function navmap(elem){
+    switch(elem.id){
+        case "products":
+            nav_redirect_url = "products.html";
+            break;
+        case "service":
+            nav_redirect_url = "service.html";
+            break;
+        case "clientele":
+            nav_redirect_url = "about.html";
+            break;
+        case "contact":
+            nav_redirect_url = "contact.html";
+            break;
+        default:
+            nav_redirect_url = "index.html";
+    }
+}
+
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+  }
